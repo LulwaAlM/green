3dd473a71fec8deda9cb4fa7a183e08a3a5f7eaf
LulwaAlM <Lulwa.almannaei@polytechnic.bh>
2025-10-07 11:50:15 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.getElementById("contactForm").onsubmit = function () {
+  alert("Your inquiry has been submitted. ");
+  return false; 
+};
+
+document.getElementsByTagName("tr")[1].onclick = function () {
+  alert("Tutor Info:\nDate: Monday\nTime: 10 - 11 PM\nLocation: B20.104");
+};
+
+document.getElementsByTagName("tr")[2].onclick = function () {
+  alert("Tutor Info:\nDate: Thursday\nTime: 12 - 1 PM\nLocation: B20.104");
+};
