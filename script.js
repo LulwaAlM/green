6e0e7fef2667daa96bcc4eb25a3a431d285b8915
LulwaAlM <Lulwa.almannaei@polytechnic.bh>
2025-10-11 19:56:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-document.getElementById("contactForm").onsubmit = function () {
-  alert("Your inquiry has been submitted. ");
-  return false; 
-};
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the actual form submission
+  alert("Your inquiry has been submitted.");
+})
 
 document.getElementsByTagName("tr")[1].onclick = function () {
   alert("Tutor Info:\nDate: Monday\nTime: 10 - 11 PM\nLocation: B20.104");
